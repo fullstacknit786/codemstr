a8d4ba17888a6ba17dcac5b8c65221a26ce907ec
Nitin <nitin.singh7570@gmail.com>
2019-11-27 14:29:37 +0530

    file deleted

--- diskStorage.js
+++
@@ -1,9 +0,0 @@
-import { Platform } from 'react-native';
-const RNFS = require('react-native-fs');
-
-export const dirHome = Platform.select({
-    android: `${RNFS.ExternalStorageDirectoryPath}/myAppName`
-});
-
-export const dirPicutures = `${RNFS.ExternalStorageDirectoryPath}/Pictures`;
-//export const dirAudio = `${dirHome}/Audio`;
